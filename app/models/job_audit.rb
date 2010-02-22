6ef22c204d62c2c60d1272ad6c73b6028a7295e6
David Austin <daustin@MEATH.GCRC.UPENN.EDU>
2010-02-22 13:51:16 -0500

    Updated  usage report

--- app/models/job_audit.rb
+++ app/models/job_audit.rb
@@ -1,7 +1,35 @@
 class JobAudit < ActiveRecord::Base
 
 
+  def raw_file_count
+  
+    inputfiles = 0
+    
+    j = Job.find(self.job_id)
+    return -1 if j.nil?
+    script = j.runscript
 
+    # now enumerate through each line
+
+
+    script.each do |line|
+      # check for raw files
+       if (line.downcase =~ /scp\s\/mnt\/san\/lims1\/data_files.+\.raw\sproteomics\@sequest/) then
+         inputfiles = inputfiles + 1
+       end
+       #check for mzmxls
+       if (line.downcase =~ /scp\s\/mnt\/san\/lims1\/data_files.+\.mzxml\sproteomics\@sequest/) then
+
+         inputfiles = inputfiles + 1
+
+       end
+
+
+    end
+
+    return inputfiles
+
+  end
 
 
 end
